d39d5dfa8660e2faf4f53d5ac489e0751f3ca313
mike oguma <mikeoguma26@gmail.com>
2023-10-17 15:18:18 +0300

    started the backend

---
+++ express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define routes and middleware here
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+  
