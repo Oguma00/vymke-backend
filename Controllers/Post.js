f0c16e7e1bab314f8f97c321b6b19cf51854f8f9
mike oguma <mikeoguma36@gmail.com>
2023-10-23 15:17:17 +0300

    adding fundi

--- Controllers/Post.js
+++ Controllers/Post.js
@@ -1,9 +1,9 @@
-// const mongoose = require("mongoose");
-// const postSchema=new mongoose.Schema({
-//     fundiImg:{type:String,required:true},
-//     fundiBody:{type:String,required:true},
-//     fundiCategory:{type:String,required:true},
-//     fundiAuthor:{type:String,required:true},
+const mongoose = require("mongoose");
+const postSchema=new mongoose.Schema({
+    fundiImg:{type:String,required:true},
+    fundiBody:{type:String,required:true},
+    fundiCategory:{type:String,required:true},
+    fundiAuthor:{type:String,required:true},
 
-// })
-// module.exports=mongoose.model("fundi", postSchema)
+})
+module.exports=mongoose.model("fundi", postSchema)
--- routes/fundi.js
+++ routes/fundi.js
@@ -1,22 +1,22 @@
-// const {
-//   addFundi,
-//   getFundis,
-//   getFundi,
-//   updateFundi,
-//   deleteFundi,
-// } = require("../controllers/fundi");
-// const Fundi = require("../models/post");
-// const express = require("express");
-// const router = express.Router();
-// // Adding Fundis
-// router.post("/new", addFundi);
-// // Get all Fundis
-// router.get("/", getFundis);
-// // Get single Fundi
-// router.get("/:id", getFundi);
-// // Patch single Fundi
-// router.patch("/update/:id", updateFundi);
-// // Delete  Fundi
-// router.delete("/delete/:id", deleteFundi);
+const {
+  addFundi,
+  getFundis,
+  getFundi,
+  updateFundi,
+  deleteFundi,
+} = require("../controllers/fundi");
+const Fundi = require("../models/post");
+const express = require("express");
+const router = express.Router();
+// Adding Fundis
+router.post("/new", addFundi);
+// Get all Fundis
+router.get("/", getFundis);
+// Get single Fundi
+router.get("/:id", getFundi);
+// Patch single Fundi
+router.patch("/update/:id", updateFundi);
+// Delete  Fundi
+router.delete("/delete/:id", deleteFundi);
 
-// module.exports = router;
+module.exports = router;
