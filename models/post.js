cb9b5054bb5fc3868868bef76beaac0447321e4a
mike oguma <mikeoguma36@gmail.com>
2023-10-27 09:31:52 +0300

    added alot of changes

--- models/post.js
+++ models/post.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-const postSchema=new mongoose.Schema({
-    postImg:{type:String,required:true},
-    postProfession:{type:String,required:true},
-    postContact:{type:String,required:true},
-    postBooking:{type:String,required:true},
-    postName:{type:String,required:true},
-
-})
-module.exports=mongoose.model("Post", postSchema)
+const postSchema = new mongoose.Schema({
+  postImg: { type: String, required: true },
+  postProfession: { type: String, required: true },
+  postBooking: { type: String, required: true },
+  postName: { type: String, required: true },
+});
+module.exports = mongoose.model("Post", postSchema);
